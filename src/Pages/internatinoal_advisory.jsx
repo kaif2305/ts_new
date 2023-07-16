56c6674769bb133873eb17820db2a85160ff0b87
asmiagarwal <agarwal.asmi@gmail.com>
2023-07-16 18:43:24 +0530

    update iac

--- src/Pages/internatinoal_advisory.jsx
+++ src/Pages/internatinoal_advisory.jsx
@@ -108,6 +108,439 @@ export default function InternatinoalAdvisory() {
                             </div>
                         </div>
 
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Carlo Ingrao.jpeg"/>
+                                <div>
+                                    <h4>Prof. Carlo Ingrao</h4>
+                                    <h3>ITALY</h3>
+                                    <h5>University of Bari Aldo Moro</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Chi-Wang Li.jpeg" />
+                                <div>
+                                    <h4>Prof. Chi-Wang Li</h4>
+                                    <h3>TAIWAN</h3>
+                                    <h5>Tamkang University</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Dhanasekar R..jpeg" />
+                                <div>
+                                    <h4>Prof. Dhanasekar R.</h4>
+                                    <h3>INDIA</h3>
+                                    <h5>Annamalai University</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Gerold Rahmann.jpeg" />
+                                <div>
+                                    <h4>Prof. Gerold Rahmann</h4>
+                                    <h3>GERMANY</h3>
+                                    <h5>Johann Heinrich von Thünen Institute</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Hau Thi Nguyen.jpeg" />
+                                <div>
+                                    <h4>Prof. Hau Thi Nguyen</h4>
+                                    <h3>VIETNAM</h3>
+                                    <h5>Dalat University</h5>
+
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Hem Raj Pant.jpeg" />
+                                <div>
+                                    <h4>Prof. Hem Raj Pant</h4>
+                                    <h3>NEPAL</h3>
+                                    <h5>Tribhuvan University</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Hideaki Yoshitake.jpeg" />
+                                <div>
+                                    <h4>Prof. Hideaki Yoshitake</h4>
+                                    <h3>JAPAN</h3>
+                                    <h5>Yokohama National University</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\I Made Joni.jpeg" />
+                                <div>
+                                    <h4>Prof. I Made Joni</h4>
+                                    <h3>INDONESIA</h3>
+                                    <h5>Universitas Padjadjaran</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Jennifer P. Tamayo.jpeg" />
+                                <div>
+                                    <h4>Dr. Jennifer P. Tamayo</h4>
+                                    <h3>PHILIPPINES</h3>
+                                    <h5>Forest Products Research and Development Institute</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Jih-Hsing Chang.jpeg" />
+                                <div>
+                                    <h4>Prof. Jih-Hsing Chang</h4>
+                                    <h3>TAIWAN</h3>
+                                    <h5>Chaoyang University of Technology</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Manikandan Vasudevan.jpeg" />
+                                <div>
+                                    <h4>Mr. Manikandan Vasudevan</h4>
+                                    <h3>INDIA</h3>
+                                    <h5>Hydranautics Inc.</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Md Shafiur Rahman.jpeg" />
+                                <div>
+                                    <h4>Prof. Md Shafiur Rahman</h4>
+                                    <h3>OMAN</h3>
+                                    <h5>Sultan Qaboos University</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Mohammed J.K. Bashir.jpeg" />
+                                <div>
+                                    <h4>Prof. Mohammed J.K. Bashir</h4>
+                                    <h3>MALAYSIA</h3>
+                                    <h5>Universiti Tunku Abdul Rahman</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Mohammed Shaad Ansari.jpeg" />
+                                <div>
+                                    <h4>Prof. Mohammed Shaad Ansari</h4>
+                                    <h3>SWEDEN</h3>
+                                    <h5>Linköping University</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Mukesh Upadhyay - 2.jpeg" />
+                                <div>
+                                    <h4>Prof. Mukesh Upadhyay</h4>
+                                    <h3>IRELAND</h3>
+                                    <h5>University of Limerick</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Murugesan Thanabalan.jpeg" />
+                                <div>
+                                    <h4>Prof. Murugesan Thanabalan</h4>
+                                    <h3>INDIA</h3>
+                                    <h5>B. S. Abdur Rahman Crescent Institute of Science and Technology</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Mushtaque Ahmed.jpeg" />
+                                <div>
+                                    <h4>Prof. Mushtaque Ahmed</h4>
+                                    <h3>OMAN</h3>
+                                    <h5>Sultan Qaboos University</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Nandita Dasgupta.jpeg" />
+                                <div>
+                                    <h4>Dr. Nandita Dasgupta</h4>
+                                    <h3>SPAIN</h3>
+                                    <h5>Universidad de Burgos</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Nguyen Cong Nguyen.jpeg" />
+                                <div>
+                                    <h4>Prof. Nguyen Cong Nguyen</h4>
+                                    <h3>VIETNAM</h3>
+                                    <h5>Dalat University</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Raghuram Chetty.jpeg" />
+                                <div>
+                                    <h4>Prof. Raghuram Chetty</h4>
+                                    <h3>INDIA</h3>
+                                    <h5>Indian Institute of Technology Madras</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Rajan Rathinasabapathy.jpeg" />
+                                <div>
+                                    <h4>Dr. Rajan Rathinasabapathy</h4>
+                                    <h3>USA</h3>
+                                    <h5>Phillips 66</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Ramachandran K P.jpeg" />
+                                <div>
+                                    <h4>Prof. Ramachandran K P</h4>
+                                    <h3>OMAN</h3>
+                                    <h5>National University of Science & Technology</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Ramani Kannan.jpeg" />
+                                <div>
+                                    <h4>Prof. Ramani Kannan</h4>
+                                    <h3>MALAYSIA</h3>
+                                    <h5>University Teknologi PETRONAS</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Randeep Singh.jpeg" />
+                                <div>
+                                    <h4>Dr. Randeep Singh</h4>
+                                    <h3>SOUTH KOREA</h3>
+                                    <h5>Yeungnam University</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Reddy Prasad D.M..jpeg" />
+                                <div>
+                                    <h4>Prof. Reddy Prasad D.M.</h4>
+                                    <h3>BRUNEI</h3>
+                                    <h5>Universiti Teknologi Brunei</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Saikat Sinha Ray.jpeg" />
+                                <div>
+                                    <h4>Dr. Saikat Sinha Ray</h4>
+                                    <h3>SOUTH KOREA</h3>
+                                    <h5>Ulsan National Institute of Science Technology</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Sakthiaswaran Kaliappan.jpeg" />
+                                <div>
+                                    <h4>Ir. Ts. Sakthiaswaran Kaliappan</h4>
+                                    <h3>MALAYSIA</h3>
+                                    <h5>Jabatan Kerja Raya</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Sean Rigby.jpeg" />
+                                <div>
+                                    <h4>Prof. Sean Rigby</h4>
+                                    <h3>UNITED KINGDOM</h3>
+                                    <h5>University of Nottingham</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Sekar S. K..jpeg" />
+                                <div>
+                                    <h4>Prof. Sekar S. K.</h4>
+                                    <h3>INDIA</h3>
+                                    <h5>Vellore Institute of Technology</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Shiao-Shing Chen.jpeg" />
+                                <div>
+                                    <h4>Prof. Shiao-Shing Chen</h4>
+                                    <h3>TAIWAN</h3>
+                                    <h5>National Taipei University of Technology</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Shimelis Kebede Kassahun.jpeg" />
+                                <div>
+                                    <h4>Prof. Shimelis Kebede Kassahun</h4>
+                                    <h3>ETHIOPIA</h3>
+                                    <h5>Addis Ababa Institute of Technology</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Shivendu Ranjan.jpeg" />
+                                <div>
+                                    <h4>Prof. Shivendu Ranjan</h4>
+                                    <h3>INDIA</h3>
+                                    <h5>Indian Institute of Technology Kharagpur</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Sylvain Usher.jpeg" />
+                                <div>
+                                    <h4>Mr. Sylvain Usher</h4>
+                                    <h3>CÔTE D’IVOIRE</h3>
+                                    <h5>African Water Association</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\TSK Sharma.jpeg" />
+                                <div>
+                                    <h4>Dr. TSK Sharma</h4>
+                                    <h3>SOUTH KOREA</h3>
+                                    <h5>University of Ulsan</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Wan Abd Al Qadr Imad Wan Mohtar.jpeg" />
+                                <div>
+                                    <h4>Prof. Wan Abd Al Qadr Imad Wan Mohtar</h4>
+                                    <h3>MALAYSIA</h3>
+                                    <h5>Universiti Malaya</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="outer-card">
+                            <div className="inter-card">
+                                <img src="images\prof\Watumesa Agustina Tan.jpeg" />
+                                <div>
+                                    <h4>Prof. Watumesa Agustina Tan</h4>
+                                    <h3>INDONESIA</h3>
+                                    <h5>Universitas Katolik Indonesia Atma Jaya</h5>
+
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
 
 
