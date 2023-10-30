2a1dc6d20ff7816c93ccb4bd005210b5a2e1301f
Kaif Ahmad <kaifhesham@gmail.com>
2023-10-31 00:37:56 +0530

    changes13

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Carousel from "./carousel";
 
 import { Countdown } from "./countdown";
+import doc1 from "../../Static/documents/Special issue procedure - Environmental Science and Pollution Research.docx";
 
 import ColabClgHome from "../Header/colab_clg_home";
 
@@ -16,7 +17,12 @@ export default function Home() {
       <div className="marq">
         <marquee behavior="slider" width="100%" direction="left">
           {" "}
-          Full Paper Submission deadline is 31<sup>st</sup> October 2023.
+          Authors can now submit their full papers to the journal.
+          &nbsp;&nbsp;&nbsp; Procedure regarding the same can be found using
+          this{" "}
+          <a href={doc1} target="_blank" style={{ color: "red" }}>
+            link
+          </a>
         </marquee>
       </div>
       <Countdown />
