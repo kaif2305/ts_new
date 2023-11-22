352c10a23ff3e35b1e26928b92fef731b84853e5
Kaif Ahmad <kaifhesham@gmail.com>
2023-11-22 17:02:40 +0530

    changes2345

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -10,6 +10,8 @@ import "../../Static/css/home.css";
 
 import visa from "../../Static/documents/MHA-CONFERENCE VISA-NOC.pdf";
 
+import letter from "../../Static/documents/VIT_0001.pdf";
+
 import Button from "react-bootstrap/Button";
 
 export default function Home() {
@@ -22,18 +24,36 @@ export default function Home() {
           Registration is now closed. Thank you for the overwhelming response.
         </marquee>
         <h4>
-          Conference Visa - NOC from Ministry of Home Affairs, Government of
-          India
+          MINSTRY OF EXTERNAL AFFAIRS (MEA) -{" "}
+          {/* <Button
+            className="btn mb-3"
+            variant="outline-primary"
+            target="_blank"
+            href={letter}
+            size="lg">
+            LETTER FOR CONFERENCE VISA
+          </Button> */}
+          <a href={letter} target="_blank">
+            LETTER FOR CONFERENCE VISA
+          </a>
         </h4>
 
-        <Button
+        <h4>
+          Conference Visa -{" "}
+          <a href={visa} target="_blank">
+            NOC from Ministry of Home Affairs, Government of India
+          </a>
+        </h4>
+
+        {/* <Button
           className="btn btn-primary mb-3"
           target="_blank"
           href={visa}
           size="lg">
           Conference Visa Notification from MHA, GoI
-        </Button>
+        </Button> */}
       </div>
+
       <Countdown />
     </div>
   );
